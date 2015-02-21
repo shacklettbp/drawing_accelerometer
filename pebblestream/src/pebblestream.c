18e02d0cd5348f825e573a0b026eebf53f0c4344
Kristen Law <kristenlaw.2014@gmail.com>
2015-02-21 02:14:15 -0800

    uncommented stuff

--- pebblestream/src/pebblestream.c
+++ pebblestream/src/pebblestream.c
@@ -68,13 +68,13 @@ static void out_sent_handler(DictionaryIterator *iter, void *context)
 	send_next_data();
 
 	// //Show on watch
-	// static char buffs[3][32];
-	// snprintf(buffs[0], sizeof("X: XXXXX"), "X: %d", latest_data[0]);
-	// snprintf(buffs[1], sizeof("Y: YYYYY"), "Y: %d", latest_data[1]);
-	// snprintf(buffs[2], sizeof("Z: ZZZZZ"), "Z: %d", latest_data[2]);
-	// text_layer_set_text(x_layer, buffs[0]);
-	// text_layer_set_text(y_layer, buffs[1]);
-	// text_layer_set_text(z_layer, buffs[2]);
+	static char buffs[3][32];
+	snprintf(buffs[0], sizeof("X: XXXXX"), "X: %d", latest_data[0]);
+	snprintf(buffs[1], sizeof("Y: YYYYY"), "Y: %d", latest_data[1]);
+	snprintf(buffs[2], sizeof("Z: ZZZZZ"), "Z: %d", latest_data[2]);
+	text_layer_set_text(x_layer, buffs[0]);
+	text_layer_set_text(y_layer, buffs[1]);
+	text_layer_set_text(z_layer, buffs[2]);
 }
 
 static void process_tuple(Tuple *t)
